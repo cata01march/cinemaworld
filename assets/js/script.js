dac8bb09d1633cc5c3b96e117a4255b4614bd119
cata01march <paul.vasilescu01@e-uvt.ro>
2023-07-28 13:15:44 +0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,75 @@
+'use strict';
+
+/**
+ * navbar variables
+ */
+
+const navOpenBtn = document.querySelector("[data-menu-open-btn]");
+const navCloseBtn = document.querySelector("[data-menu-close-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+for (let i = 0; i < navElemArr.length; i++) {
+
+  navElemArr[i].addEventListener("click", function () {
+
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("active");
+
+  });
+
+}
+
+
+
+/**
+ * header sticky
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+
+  window.scrollY >= 10 ? header.classList.add("active") : header.classList.remove("active");
+
+});
+
+
+
+/**
+ * go top
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  window.scrollY >= 500 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
+
+});
+
+/**
+ * search
+ */
+
+const searchInputWrapper = document.querySelector(".search-input-wrapper");
+const searchInput = document.querySelector(".search-input input");
+const searchIcon = document.querySelector(".search-icon i");
+const closeIcon = document.querySelector(".search-input i ");
+
+searchIcon.addEventListener("click", () => {
+  searchIcon.parentElement.classList.add("change");
+  searchInputWrapper.classList.add("change");
+
+  setTimeout(() => {
+    searchInput.focus();
+  }, 1000);
+});
+
+closeIcon.addEventListener("click", () => {
+  searchIcon.parentElement.classList.remove("change");
+  searchInputWrapper.classList.remove("change");
+});
---
+++ logout.php
@@ -0,0 +1,28 @@
+<?php
+error_reporting(E_ALL); ini_set('display_errors', 1);
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Establish database connection
+    $host = 'localhost';
+    $user = 'root';
+    $password = '';
+    $database = 'user_db'; 
+
+    $connection = new mysqli($host, $user, $password, $database);
+
+    // Check connection
+    if ($connection->connect_error) {
+        die("Connection failed: " . $connection->connect_error);
+    }
+    
+}
+// Destroy the session and unset the session variables
+$_SESSION = [];
+session_unset();
+session_destroy();
+
+header("Location: signin.html");
+exit();
+
+?>
